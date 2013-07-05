ce42f65be2dfe6be011c32d4424259267a7a80bd
jbradmil <>
2013-07-05 16:15:56 +0000

    cleanup of lvdbmon

--- source/odmb_vme/lvdbmon.vhd
+++ source/odmb_vme/lvdbmon.vhd
@@ -1,7 +1,10 @@
 library ieee;
 library work;
-use work.Latches_Flipflops.all;
+library unisim;
+use unisim.vcomponents.all;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.hdlmacro.all;
 
 entity LVDBMON is
   
@@ -57,47 +60,44 @@ architecture LVDBMON_Arch of LVDBMON is
   signal CE_OUTDATA_FULL : std_logic;
   signal Q_OUTDATA_FULL : std_logic_vector(15 downto 0);
   signal SLI_ADCDATA,L_ADCDATA,CE_ADCDATA : std_logic;
-  signal D_ADCDATA,Q_ADCDATA : std_logic_vector(7 downto 0);
-  signal CMDDEV : std_logic_vector(4 downto 0);
+  signal D_ADCDATA,Q_ADCDATA: std_logic_vector(7 downto 0);
+  signal CMDDEV : unsigned(12 downto 0);
   
 begin  --Architecture
 
 -- Decode instruction
-  CMDHIGH <= '1' when (COMMAND(9) = '0' and COMMAND(8) = '0' and COMMAND(7) = '0' and COMMAND(6) = '0'
-                       and COMMAND(5) = '0' and COMMAND(4) = '0' and DEVICE = '1') else '0';
-  CMDDEV <= CMDHIGH & COMMAND(3) & COMMAND(2) & COMMAND(1) & COMMAND(0);
-
-  WRITEADC        <= '1' when (CMDDEV = "10000") else '0';
-  READMON         <= '1' when (CMDDEV = "10001") else '0';
-  WRITEPOWER      <= '1' when (CMDDEV = "10100") else '0';
-  READPOWER       <= '1' when (CMDDEV = "10101") else '0';
-  READPOWERSTATUS <= '1' when (CMDDEV = "10110") else '0';
-  SELADC          <= '1' when (CMDDEV = "11000") else '0';
-  READADC         <= '1' when (CMDDEV = "11001") else '0';
+  CMDHIGH <= '1' when (COMMAND(9 downto 4) = "000000" and DEVICE = '1') else '0';
+  CMDDEV <= unsigned(CMDHIGH & "000000" & COMMAND(3 downto 0) & "00");
+  
+  WRITEADC        <= '1' when (CMDDEV = x"1000") else '0';
+  READMON         <= '1' when (CMDDEV = x"1004") else '0';
+  WRITEPOWER      <= '1' when (CMDDEV = x"1010") else '0';
+  READPOWER       <= '1' when (CMDDEV = x"1014") else '0';
+  READPOWERSTATUS <= '1' when (CMDDEV = x"1018") else '0';
+  SELADC          <= '1' when (CMDDEV = x"1020") else '0';
+  READADC         <= '1' when (CMDDEV = x"1024") else '0';
 
 -- Generate OUTDATA
-  FDCE(INDATA(0),STROBE,SELADC,RST,SELADC_vector(1));
-  FDCE(INDATA(1),STROBE,SELADC,RST,SELADC_vector(2));
-  FDCE(INDATA(2),STROBE,SELADC,RST,SELADC_vector(3));
+  FDCE_GEN: for i in 0 to 2 generate
+  begin
+  	FDCE_OUT : FDCE port map (SELADC_vector(i+1),STROBE,SELADC,RST,INDATA(i));
+  end generate FDCE_GEN;	
+  
   OUTDATA <= "0000000000000" & SELADC_vector(3 downto 1) when (STROBE='1' and READADC='1') else (others => 'Z');
   D_OUTDATA <= '1' when (STROBE='1' and READADC='1') else '0';
-  FD(D_OUTDATA,SLOWCLK,Q_OUTDATA);
+  FD_OUTDATA : FD port map (Q_OUTDATA,SLOWCLK,D_OUTDATA);
   DTACK_INNER <= '0' when (Q_OUTDATA='1') else 'Z';
 
 -- Generate DTACK_2
   D_DTACK_2 <= SELADC and STROBE;
-  FD(D_DTACK_2,SLOWCLK,Q_DTACK_2);
+  FD_DTACK_2 : FD port map (Q_DTACK_2,SLOWCLK,D_DTACK_2);
   DTACK_INNER <= '0' when (Q_DTACK_2='1') else 'Z';
 
 -- Generate OUTDATA_2
-  FDCE(INDATA(0),STROBE,WRITEPOWER,RST,LVTURNON_INNER(1));
-  FDCE(INDATA(1),STROBE,WRITEPOWER,RST,LVTURNON_INNER(2));
-  FDCE(INDATA(2),STROBE,WRITEPOWER,RST,LVTURNON_INNER(3));
-  FDCE(INDATA(3),STROBE,WRITEPOWER,RST,LVTURNON_INNER(4));
-  FDCE(INDATA(4),STROBE,WRITEPOWER,RST,LVTURNON_INNER(5));
-  FDCE(INDATA(5),STROBE,WRITEPOWER,RST,LVTURNON_INNER(6));
-  FDCE(INDATA(6),STROBE,WRITEPOWER,RST,LVTURNON_INNER(7));
-  FDCE(INDATA(7),STROBE,WRITEPOWER,RST,LVTURNON_INNER(8));
+  FDCE_GEN2: for i in 0 to 7 generate
+  begin
+  	FDCE_OUT2 : FDCE port map (LVTURNON_INNER(i+1),STROBE,WRITEPOWER,RST,INDATA(i));
+  end generate FDCE_GEN2;
 
   OUTDATA <= x"00" & LVTURNON_INNER(8 downto 1) when (STROBE='1' and READPOWER='1') else
                           x"00" & R_LVTURNON(8 downto 1) when (STROBE='1' and READPOWERSTATUS='1') else
@@ -106,12 +106,12 @@ begin  --Architecture
                  '1' when (STROBE='1' and READPOWERSTATUS='1') else
                  '0';
 
-  FD(D_OUTDATA_2,SLOWCLK,Q_OUTDATA_2);
+  FD_OUTDATA_2 : FD port map (Q_OUTDATA_2,SLOWCLK,D_OUTDATA_2);
   DTACK_INNER <= '0' when (Q_OUTDATA_2='1') else 'Z';
   
 -- Generate DTACK_4
   D_DTACK_4 <= '1' when (WRITEPOWER='1' and STROBE='1') else '0';
-  FD(D_DTACK_4,SLOWCLK,Q_DTACK_4);
+  FD_DTACK_4 : FD port map (Q_DTACK_4,SLOWCLK,D_DTACK_4);
   DTACK_INNER <= '0' when (Q_DTACK_4='1') else 'Z';
 
 -- Generate RDMONBK
@@ -129,55 +129,55 @@ begin  --Architecture
 
 -- Generate LOADON
   C_LOADON <= '1' when (WRITEPOWER='1' and STROBE='1') else '0';
-  FDC(VCC,C_LOADON,LOADON_INNER,Q1_LOADON);
-  FD(Q1_LOADON,SLOWCLK,Q2_LOADON);
-  FD(Q2_LOADON,SLOWCLK,LOADON_INNER);
+  FDC_LOADON : FDC port map (Q1_LOADON,C_LOADON,LOADON_INNER,VCC);
+  FD_LOADON1 : FD port map (Q2_LOADON,SLOWCLK,Q1_LOADON);
+  FD_LOADON2 : FD port map (LOADON_INNER,SLOWCLK,Q2_LOADON);
 
 -- Generate OUTDATA
   CE_OUTDATA_FULL <= '1' when (BUSY='1' and RSTBUSY='0' and CLKMON='0') else '0';
-  SR16CE(SLOWCLK,CE_OUTDATA_FULL,RST,ADCIN,Q_OUTDATA_FULL,Q_OUTDATA_FULL);
+  SR16CE_OUTDATA : SR16CE port map (Q_OUTDATA_FULL,SLOWCLK,CE_OUTDATA_FULL,RST,ADCIN);
   OUTDATA(15 downto 0) <= Q_OUTDATA_FULL(15 downto 0) when (RDMONBK='1') else
-                          "ZZZZZZZZZZZZZZZZ";
+                          (others => 'Z');
   SLI_ADCDATA <= 'L';
   
 
 -- Generate ADCDATA
   L_ADCDATA <= '1' when (LOAD='1' and CLKMON='0') else '0';
   CE_ADCDATA <= '1' when (BUSY='1' and CLKMON='0') else '0';
-  SR8CLE(SLOWCLK,CE_ADCDATA,RST,L_ADCDATA,SLI_ADCDATA,INDATA(7 downto 0),Q_ADCDATA,Q_ADCDATA);
+  SR8CLE_ADCDATA : SR8CLE port map (Q_ADCDATA,SLOWCLK,CE_ADCDATA,RST,INDATA(7 downto 0),L_ADCDATA,SLI_ADCDATA);
   ADCDATA <= Q_ADCDATA(7);
 
 -- Generate ADCCLK
   CE_ADCCLK <= '1' when (BUSY='1' and RSTBUSY='0') else '0';
   CLR_ADCCLK <= '1' when (BUSY='0' or RST='1') else '0';
-  FDCE(ADCCLK_INNER,SLOWCLK,CE_ADCCLK,CLR_ADCCLK,CLKMON);     
+  FDCE_ADCCLK : FDCE port map (CLKMON,SLOWCLK,CE_ADCCLK,CLR_ADCCLK,ADCCLK_INNER);     
   ADCCLK_INNER <= not CLKMON;
 
 -- Generate BUSY
   CE1_BUSY <= '1' when (BUSY='1' and CLKMON='0') else '0';
   CLR_BUSY <= Q2_BUSY or RST;
-  CB8CE(SLOWCLK,CE1_BUSY,CLR_BUSY,QTIME,QTIME,blank1,blank2);
+  CB8CE_BUSY : CB8CE port map (blank1,QTIME,blank2,SLOWCLK,CE1_BUSY,CLR_BUSY);
   DONEMON <= '1' when (QTIME(4)='1' and QTIME(3)='1' and QTIME(1)='1') else '0';
   CE2_BUSY <= BUSY and CLKMON;
-  FDCE(DONEMON,SLOWCLK,CE2_BUSY,CLR_BUSY,Q1_BUSY);
-  FD(Q1_BUSY,SLOWCLK,Q2_BUSY);
+  FDCE_BUSY : FDCE port map (Q1_BUSY,SLOWCLK,CE2_BUSY,CLR_BUSY,DONEMON);
+  FD_BUSY : FD port map(Q2_BUSY,SLOWCLK,Q1_BUSY);
   RSTBUSY <= RST or Q1_BUSY;
   D_BUSY <= LOAD or BUSY;
-  FDR(D_BUSY,SLOWCLK,RSTBUSY,BUSY);
+  FDR_BUSY : FDR port map (BUSY,SLOWCLK,D_BUSY,RSTBUSY);
 
 -- Generate LOAD
   ASYNLOAD <= '1' when (STROBE='1' and WRITEADC='1' and BUSY='0') else '0';
   CLR1_LOAD <= RST or Q2_LOAD;
-  FDC(VCC,ASYNLOAD,CLR1_LOAD,Q1_LOAD);
-  FDC(Q1_LOAD,SLOWCLK,RST,LOAD);
+  FDC_VCC : FDC port map (Q1_LOAD,ASYNLOAD,CLR1_LOAD,VCC);
+  FDC_LOAD1 : FDC port map (LOAD,SLOWCLK,RST,Q1_LOAD);
   CE_LOAD <= '1' when (BUSY='1' and CLKMON='0') else '0';
-  FDCE(LOAD,SLOWCLK,CE_LOAD,RST,Q2_LOAD);
-  FDC(Q2_LOAD,SLOWCLK,RST,Q3_LOAD);
+  FDCE_LOAD2 : FDCE port map (Q2_LOAD,SLOWCLK,CE_LOAD,RST,LOAD);
+  FDC_LOAD3 : FDC port map (Q3_LOAD,SLOWCLK,RST,Q2_LOAD);
 
 -- Guido: to bring DTACK high after the end of the ADC acquisition (otherwise we need to wait for another code)
 --    if (RST='1' or WRITEADC='0') then
   CLR2_LOAD <= '1' when (RST='1' or WRITEADC='0' or BUSY='0') else '0';
-  FDC(VCC,Q3_LOAD,CLR2_LOAD,Q4_LOAD);
+  FDC_LOAD4 : FDC port map (Q4_LOAD,Q3_LOAD,CLR2_LOAD,VCC);
   DTACK_INNER <= '0' when (Q4_LOAD='1') else 'Z';
 
 -- Generate LOADON / Generate DTACK / Generate LVTURNON / Generate ADCLK
@@ -187,12 +187,6 @@ begin  --Architecture
   ADCCLK <= ADCCLK_INNER;
 
 -- Generate DIAGLVDB
-  DIAGLVDB(0) <= SLOWCLK;
-  DIAGLVDB(1) <= CE_ADCDATA;
-  DIAGLVDB(2) <= CLKMON;
-  DIAGLVDB(3) <= ADCCLK_INNER;
-  DIAGLVDB(4) <= BUSY;
-  DIAGLVDB(5) <= L_ADCDATA;
-  DIAGLVDB(17 downto 6) <= "000000000000";   
+  DIAGLVDB(17 downto 0) <= "000000000000" & L_ADCDATA & BUSY & ADCCLK_INNER & CLKMON & CE_ADCDATA & SLOWCLK;
   
 end LVDBMON_Arch;
