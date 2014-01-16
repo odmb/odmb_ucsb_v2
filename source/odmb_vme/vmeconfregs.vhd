468abdd7c7503ea8877d6a6991eedf4b893df6fa
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2014-01-16 21:11:13 +0100

    Reverted the event handler for Tag V02-03

--- source/odmb_vme/vmeconfregs.vhd
+++ source/odmb_vme/vmeconfregs.vhd
@@ -69,9 +69,9 @@ end VMECONFREGS;
 
 architecture VMECONFREGS_Arch of VMECONFREGS is
 
-  constant FW_VERSION       : std_logic_vector(15 downto 0) := x"0202";
-  constant FW_ID            : std_logic_vector(15 downto 0) := x"0006";
-  constant FW_MONTH_DAY     : std_logic_vector(15 downto 0) := x"0115";
+  constant FW_VERSION       : std_logic_vector(15 downto 0) := x"0203";
+  constant FW_ID            : std_logic_vector(15 downto 0) := x"0000";
+  constant FW_MONTH_DAY     : std_logic_vector(15 downto 0) := x"0116";
   constant FW_YEAR          : std_logic_vector(15 downto 0) := x"2014";
   constant able_write_const : std_logic                     := '0';
 
